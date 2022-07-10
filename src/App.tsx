f4b721aa5ba2906dc4c061dde79c2ecd95aa3853
Vladimir Zvyagin <vladimir_zvyagin@mail.ru>
2022-07-10 23:36:05 +0300

    See all videos from 0 lesson of course

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import './App.css';
+import Accordion from "./components/Accordion/Accordion";
+import {Rating} from "./components/Rating/Rating";
 
 function App() {
   return (
     <div className="App">
-     Hello, samurai! Let's go!
+        <PageTitle title={'Hello, samurai! Let\'s go!'}/>
+        <PageTitle title={'My friends'}/>
+        Article 1
+        <Rating value={4}/>
+        <Accordion titleValue={'Меню 1'} collapsedMenu={true}/>
+        <Accordion titleValue={'Меню 2'} collapsedMenu={false}/>
+        Article 2
+        <Rating value={1}/>
     </div>
   );
 }
+
+type PageTitlePropsType = {
+    title: string
+}
+
+function PageTitle(props: PageTitlePropsType) {
+    return (
+        <h1>{props.title}</h1>
+    );
+}
+
 
 export default App;
